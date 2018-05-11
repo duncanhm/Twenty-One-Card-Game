00436c457caf191594f734606a18b19fc096d317
Duncan Howard-McGuire <d.t.howardmcguire@gmail.com>
2018-05-10 17:41:58 -0700

    Added two new classes: Game and TwentyOneGame. Also refactored the Shuffle method in the Deck class.

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -10,8 +10,12 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
+            //TwentyOneGame game = new TwentyOneGame();
+            //game.Players = new List<string> { "James", "Ricky", "George", "Sally" };
+            //game.ListPlayers();
+
             Deck deck = new Deck();
-            deck = Shuffle(deck); //This is where we are calling the method/function called "Shuffle"
+            deck.Shuffle(3);
 
             foreach (Card card in deck.Cards)
             {
@@ -19,23 +23,6 @@ namespace TwentyOne
             }
             Console.WriteLine(deck.Cards.Count);
             Console.Read();
-
-        }
-
-        // creating a new method called "Shuffle"
-        public static Deck Shuffle(Deck deck) 
-        {
-            List<Card> TempList = new List<Card>();
-            Random random = new Random();
-
-            while (deck.Cards.Count > 0)
-            {
-                int randomIndex = random.Next(0, deck.Cards.Count);
-                TempList.Add(deck.Cards[randomIndex]);
-                deck.Cards.RemoveAt(randomIndex);
-            }
-            deck.Cards = TempList;
-            return deck;
         }
     }
 }
