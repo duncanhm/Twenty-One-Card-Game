109b17d743a42a28274c9841e6d1f6594290c2d4
Duncan Howard-McGuire <d.t.howardmcguire@gmail.com>
2018-05-17 14:31:36 -0700

    Changed Card class to struct.

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -10,26 +10,6 @@ namespace TwentyOne
     {
         static void Main(string[] args)
         {
-            //TwentyOneGame game = new TwentyOneGame();
-            //game.Players = new List<string> { "James", "Ricky", "George", "Sally" };
-            //game.ListPlayers();
-
-            //Game game = new TwentyOneGame();
-            //game.Players = new List<Player>();
-            //Player player = new Player();
-            //player.Name = "Jesse";
-            //game = game + player; // Could be better written as: game += player;, see below.
-            //game -= player; // DRY!
-
-            Deck deck = new Deck();
-            deck.Shuffle(3);
-
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count);
-            Console.Read();
         }
     }
 }
