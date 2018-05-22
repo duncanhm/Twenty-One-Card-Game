d15b472d9f0478ef7728d2654ac67421cfb29dec
Duncan Howard-McGuire <d.t.howardmcguire@gmail.com>
2018-05-22 11:10:26 -0700

    Added new exception method FraudException, to catch any attempts at cheating. Also added new try/catch and while loops for exception handling with money and bet amounts to Main() and TwentyOneGame class.

--- TwentyOne/Program.cs
+++ TwentyOne/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Casino;
 using Casino.TwentyOne;
 
@@ -10,19 +11,58 @@ namespace TwentyOne
         {
             Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
             string playerName = Console.ReadLine();
-            Console.WriteLine("And how much money did you bring today?");
-            int bank = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("And how much money did you bring today?");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals");
+            }
+            try // Added this myself
+            {
+                if (bank < 0)
+                {
+                    throw new FraudException();
+                }
+            }
+            catch (FraudException)
+            {
+                Console.WriteLine("Security! Kick this person out.");
+                Console.ReadLine();
+                return;
+            } // Stop personal addition
+            Console.WriteLine("Great, {0}, you have ${1} to bet with. Would you like to join a game of 21 right now?", playerName, bank);
             string answer = Console.ReadLine().ToLower();
-            if (answer == "yes" || answer == "yeah" || answer == "ya" || answer == "yea" || answer == "y" || answer == "ok" || answer == "okay")
+            if (answer == "yes" || answer == "yeah" || answer == "ya" || answer == "yea" || answer == "y" || answer == "ok" || answer == "okay" || answer == "sure" )
             {
                 Player player = new Player(playerName, bank);
+                player.Id = Guid.NewGuid();
+                using (StreamWriter file = new StreamWriter(@"D:\log\log.txt", true))
+                {
+                    file.WriteLine(player.Id);
+                }
                 Game game = new TwentyOneGame();
                 game += player;
                 player.isActivelyPlaying = true;
                 while (player.isActivelyPlaying && player.Balance > 0)
                 {
-                    game.Play();
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch (FraudException)
+                    {
+                        Console.WriteLine("Security! Kick this person out.");
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("An error occured, please contact your System Administrator.");
+                        Console.ReadLine();
+                        return;
+                    }
                 }
                 game -= player;
                 Console.WriteLine("Thank you for playing!");
